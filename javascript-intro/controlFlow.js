68afcc32a63305b75f6d39c87adddf0f9bab923b
Richard <Ryuukishi@users.noreply.github.com>
2022-05-24 21:13:57 +1000

    Add control flow

---
+++ javascript-intro/controlFlow.js
@@ -0,0 +1,10 @@
+const isSkyBlue = true;
+
+let greeting;
+
+if (isSkyBlue) {
+    greeting = 'It must be nice weather';
+} else {
+    greeting = 'It must be bad weather'   
+}
+console.log(greeting);
